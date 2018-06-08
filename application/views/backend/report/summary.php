d8ba908dc61860370c4f60bda055251f79533a58
anindayuni <anindayuni01@gmail.com>
2018-06-08 10:41:36 +0700

    summary

---
+++ application/views/backend/report/summary.php
@@ -0,0 +1,16 @@
+        <!-- page heading start-->
+        <div id="content">
+            <div id="content-header">
+                <div id="breadcrumb">
+                    <a href="<?php echo base_url('mastercms'); ?>" title="" class="tip-bottom" data-original-title="Go to Home">
+                        <i class="icon-home"></i> Home
+                    </a>
+                    <a href="<?php echo base_url('mastercms/absensi'); ?>">Absensi</i>
+                    </a>
+                </div>
+            </div>
+            <!-- page heading end-->
+            <!-- body wrapper start -->
+            
+
+        </div>
