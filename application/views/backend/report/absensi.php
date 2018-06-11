6473de73e7f23c43098aacc01a795ce4bbba204c
khoirul94 <39944640+khoirul94@users.noreply.github.com>
2018-06-11 11:29:25 +0700

    page absensi

--- application/views/backend/report/absensi.php
+++ application/views/backend/report/absensi.php
@@ -15,16 +15,52 @@
             <div class="container-fluid">
                 <div class="row-fluid">
                     <div class="span12">
-                        <form name="filterFrm" action="<?php echo base_url('mastercms/absensi/cari');?>" method="get">
-                            <div class="span2">
+                        <form name="filterFrm" class="form" method="post">
+                            <div class="span4">
                                 <div class="controls">
-                                    <input type="text" name="cari" class="span12" placeholder="Cari Data" value="" />
+                                    <select name="cabang" class="form-control" id="cabang">
+                                        <option value="">- Pilih perusahaan / cabang -</option>
+                                        <?php foreach ($perusahaan as $key => $value) :?>
+                                            <option value="<?php echo $value['lokasi_id'] ?>"><?php echo $value['lokasi_nama']; ?></option>
+                                        <?php endforeach ?>
+                                    </select>
                                 </div>
                             </div>
-                            <div class="span4" style="margin-left: 5px">
+                            <div class="span2">
+                                <div class="controls">
+                                    <select name="filterbulan" class="form-control">
+                                        <option value="">- Pilih bulan -</option>
+                                        <option value="januari">Januari</option>
+                                        <option value="februari">Februari</option>
+                                        <option value="maret">Maret</option>
+                                        <option value="april">April</option>
+                                        <option value="mei">Mei</option>
+                                        <option value="juni">Juni</option>
+                                        <option value="juli">Juli</option>
+                                        <option value="agustus">Agustus</option>
+                                        <option value="september">September</option>
+                                        <option value="oktober">Oktober</option>
+                                        <option value="november">November</option>
+                                        <option value="desember">Desember</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="span3">
+                                <div class="controls">
+                                    <select name="karyawan" class='form-control' id='karyawan'>
+                                        <option value='0'>--pilih--</option>
+
+                                         <?php foreach ($karyawan as $key => $value) :?>
+                                            <option value="<?php echo $value['karyawan_id'] ?>"><?php echo $value['karyawan_nama']; ?></option>
+                                        <?php endforeach ?>
+                                        
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="span3 text-right">
                                 <div class="controls">
                                     <p>
-                                        <button type="submit" class="btn btn-primary"><a href="<?php echo base_url('mastercms/absensi/cari'); ?>"><i class="fa fa-search"></i></a> Cari Data</button>
+                                        <a href="<?php echo base_url('mastercms/absensi/cari'); ?>"><button type="submit" class="btn btn-primary"><i class="fa fa-search"></i> Filter</button></a>
                                         <a href="<?php echo base_url('mastercms/absensi'); ?>"><button name="resetFilterCustomer" type="submit" class="btn btn-warning"><i class="fa fa-rotate-left"></i> Reset Filter</button></a>
                                     </p>
                                 </div>
@@ -46,49 +82,36 @@
                             </div>
                             <div class="widget-content nopadding">
                                 <section id="no-more-tables">
-                                    <table class="table table-data table-bordered table-hover table-condensed cf">
-                                        <thead class="DataTables_sort_wrapper" >
-                                            <tr>
-                                                <th style="background: #dedeec; font-size: 12px" width="30">No</th>
-                                                <th style="background: #dedeec; font-size: 12px" width="100">Tanggal</th>
-                                                <th style="background: #dedeec; font-size: 12px" width="200">Nama Karyawan</th>
-                                                <th style="background: #dedeec; font-size: 12px" width="130">Hari</th>
-                                                <th style="background: #dedeec; font-size: 12px" width="130">Jam Absen</th>
-                                                <th style="background: #dedeec; font-size: 12px" width="130">Jam Keluar</th>
-                                                <th style="background: #dedeec; font-size: 12px" width="130">Status</th>
-                                                <th style="background: #dedeec; font-size: 12px" width="150">Keterangan</th>
-                                                <th style="background: #dedeec; font-size: 12px" width="60">Aksi</th>
+                                   <table class="table table-data table-bordered table-hover table-condensed cf">
+                                    <thead class="DataTables_sort_wrapper" >
+                                        <tr>
+                                            <th style="background: #dedeec; font-size: 12px" width="2%">No</th>
+                                            <th style="background: #dedeec; font-size: 12px" width="15%">Hari</th>
+                                            <th style="background: #dedeec; font-size: 12px" width="15%">Tanggal</th>
+                                            <th style="background: #dedeec; font-size: 12px" width="25%">Nama Karyawan</th>
+                                            <th style="background: #dedeec; font-size: 12px" width="15%">Jam Masuk Absen</th>
+                                            <th style="background: #dedeec; font-size: 12px" width="15%">Jam Keluar Absen</th>
+                                            <th style="background: #dedeec; font-size: 12px" width="15">Status</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <?php foreach ($absensi as $key =>$value): ?>
+                                                <td data-title="No"><?php echo $key+1 ; ?></td>
+                                                <td data-title="hari"><?php echo $value['absen_hari']; ?></td>
+                                                <td data-title="tanggal"><?php echo date('d M Y', strtotime($value['tanggal'])); ?></td>
+                                                <td data-title="karyawan_nama"><strong><?php echo $value['karyawan_nama']; ?></strong></td>
+                                                <td data-title="jam masuk"><?php echo $value['jam_masuk_absen']; ?></td>
+                                                <td data-title="jam kelauar"><?php echo $value['jam_keluar_absen']; ?></td>
+                                                <td data-title="Aksi" align="center"><?php echo $value['status']; ?></td>
                                             </tr>
-                                        </thead>
-                                        <tbody>
-                                            <?php 
-                                            foreach ($absensi as $key => $value) {?>
-                                                <tr>
-                                                    <td data-title="No"><?php echo $key+1; ?></td>
-                                                    <td data-title="tanggal"><?php echo date('d M Y', strtotime($value['tanggal'])); ?></td>
-                                                    <td data-title="karyawan_nama"><strong><a href="<?php echo base_url("mastercms/absensi/detail/$value[karyawan_id]"); ?>"><?php echo $value['karyawan_nama']; ?></a></strong></td>
-                                                    <td data-title="absen_hari"><?php echo $value['absen_hari']; ?></td>
-                                                    <td data-title="jam_masuk_absen"><?php echo $value['jam_masuk_absen']; ?></td>
-                                                    <td data-title="jam_keluar_absen"><?php echo $value['jam_keluar_absen']; ?></td>
-                                                    <td data-title="status"><?php echo $value['status']; ?></td>
-                                                    <td data-title="keterangan"><?php echo $value['keterangan']; ?></td>
-                                                    <td data-title="Aksi" align="center">
-                                                        <a href="<?php echo base_url("mastercms/absensi/detail/$value[karyawan_id]"); ?>" title="Detail"><i class="fa fa-eye"></i></a> &nbsp;
-                                                        <a href="<?php echo base_url("mastercms/absensi/hapus/$value[karyawan_id]"); ?>" title="Hapus"><i class="fa fa-trash-o"></i></a> &nbsp;
-                                                    </td>
-                                                </tr>
-                                                <?php
-                                            }
-                                            ?>
-
-                                            
-
-                                        </tbody>
-                                    </table>
-                                    <div class="center" style="text-align: center">
-                                        <!-- <?php if (isset($links)) { ?>
+                                        <?php endforeach ?>
+                                    </tbody>
+                                </table>
+                                <div class="center" style="text-align: center">
+                                    <?php if (isset($links)) { ?>
                                         <?php echo $links ?>
-                                        <?php } ?> -->
+                                        <?php } ?>
                                         <?php echo $this->pagination->create_links(); ?>
                                     </div>
 
@@ -100,3 +123,32 @@
             </div>
 
         </div>
+
+        <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+        <script type="text/javascript">
+
+            $(function(){
+
+                $.ajaxSetup({
+                    type:"POST",
+                    url: "<?php echo base_url('index.php/absensi/ambil_data') ?>",
+                    cache: false,
+                });
+
+                $("#cabang").change(function(){
+
+                    var value=$(this).val();
+                    if(value>0){
+                        $.ajax({
+                            data:{modul:'karyawan',id:value},
+                            success: function(respond){
+                                $("#karyawan").html(respond);
+                            }
+                        })
+                    }
+
+                });
+
+            })
+
+        </script>
