4c57d45d8777edcc156655b7e257c1fe2c8add4f
anindayuni <anindayuni01@gmail.com>
2018-06-11 17:00:26 +0700

    push

--- application/views/backend/karyawan/detail.php
+++ application/views/backend/karyawan/detail.php
@@ -10,75 +10,66 @@
             </a>
             <a href="#" class="current">Detail
             </a>
-    </div>
         </div>
-<!-- page heading end -->
-        
-    <div class="container-fluid">
+    </div>
+    <!-- page heading end -->
+
+    <div class="container-fluid" style="min-height: 480px;">
         <div class="row-fluid">
-            <div class="col-md-4">
+            <div class="col-md-12">
                 <div class="panel">
-                    <div class="panel-body">
-                        <div class="profile-desk">
-                            <div class="profile-pic text-center" style="margin:10px;margin-bottom:30px;width:100%;height:auto;">
-                                <img alt="Logo Perusahaan" style="width: 200px; height: auto;" name="Logo Perusahaan" src="">
-                                <h3></h3>
-                               
-                            </div>
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            &nbsp; Detail Karyawan<a href="<?php echo base_url('mastercms/karyawan/edit/').$detail_data['karyawan_id']; ?>" class="pull-right" title="Edit"><i class="fa fa-pencil" style="font-size: 14px;"></i></a>
                         </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-8">
-                <div class="panel">
-                    <div class="panel-body">
-                        <div class="profile-desk">
-                            <div class="col-md-12" style="padding-top:10px;">
-                                <h3>Detail <a href="<?php echo base_url('mastercms/karyawan/edit/').$detail_data['karyawan_id']; ?>" class="pull-right" title="Edit"><i class="fa fa-pencil"></i></a></h3>
-                                <table class="table">
-                                    <tbody>
-                                        <tr>
-                                            <td style="width: 20%">Nama Karyawan </td>
-                                            <td style="width: 5%">:</td>
-                                            <td><?php echo $detail_data['karyawan_nama']; ?></td>
-                                        </tr>
-                                        <tr>
-                                            <td>Nama Perusahaan </td>
-                                            <td>:</td>
-                                            <td><?php echo $detail_data['lokasi_nama']; ?></td>
-                                        </tr>
-                                        <tr>
-                                            <td>Status</td>
-                                            <td>:</td>
-                                            <td><?php echo $detail_data['perusahaan_title']; ?></td>
-                                        </tr>
-                                        <tr>
-                                            <td>Jabatan</td>
-                                            <td>:</td>
-                                            <td><?php echo $detail_data['karyawan_jabatan']; ?></td>
-                                        </tr>
-                                        <tr>
-                                            <td>Tanggal Lahir</td>
-                                            <td>:</td>
-                                            <td><?php  ?></td>
-                                        </tr>
-                                        <tr>
-                                            <td>Email</td>
-                                            <td>:</td>
-                                            <td><?php echo $detail_data['karyawan_email']; ?></td>
-                                        </tr>
-                                        <tr>
-                                            <td>No HP</td>
-                                            <td>:</td>
-                                            <td><?php echo $detail_data['no_hp']; ?></td>
-                                        </tr>
-                                        <tr>
-                                            <td>Alamat</td>
-                                            <td>:</td>
-                                            <td><?php echo $detail_data['karyawan_alamat']; ?></td>
-                                        </tr>
-                                    </tbody>
-                                </table>
+                        <div class="panel-body">
+                            <div class="profile-desk">
+                                <div class="col-md-12" style="padding-top:10px;">
+                                    <table class="table">
+                                        <tbody>
+                                            <tr>
+                                                <td style="width: 20%">Nama Karyawan </td>
+                                                <td style="width: 5%">:</td>
+                                                <td><?php echo $detail_data['karyawan_nama']; ?></td>
+                                            </tr>
+                                            <tr>
+                                                <td>Nama Perusahaan </td>
+                                                <td>:</td>
+                                                <td><?php echo $detail_data['lokasi_nama']; ?></td>
+                                            </tr>
+                                            <tr>
+                                                <td>Status</td>
+                                                <td>:</td>
+                                                <td><?php echo $detail_data['perusahaan_title']; ?></td>
+                                            </tr>
+                                            <tr>
+                                                <td>Jabatan</td>
+                                                <td>:</td>
+                                                <td><?php echo $detail_data['karyawan_jabatan']; ?></td>
+                                            </tr>
+                                            <tr>
+                                                <td>Tanggal Lahir</td>
+                                                <td>:</td>
+                                                <td><?php  ?></td>
+                                            </tr>
+                                            <tr>
+                                                <td>Email</td>
+                                                <td>:</td>
+                                                <td><?php echo $detail_data['karyawan_email']; ?></td>
+                                            </tr>
+                                            <tr>
+                                                <td>No HP</td>
+                                                <td>:</td>
+                                                <td><?php echo $detail_data['no_hp']; ?></td>
+                                            </tr>
+                                            <tr>
+                                                <td>Alamat</td>
+                                                <td>:</td>
+                                                <td><?php echo $detail_data['karyawan_alamat']; ?></td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
                         </div>
                     </div>
