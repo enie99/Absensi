efbf079213b11e978d3a716ffc364f994f3a116c
danis16 <daniswara.daniswara@gmail.com>
2018-07-19 15:35:51 +0700

    commit index

--- application/views/backend/index.php
+++ application/views/backend/index.php
@@ -5,6 +5,9 @@
     <!-- <link rel="shortcut icon" href="<?/*php echo base_url();*/?>/images/favicon.png" type="image/png">-->
     
     <title>SMOP - Sistem Manajemen Otret Presensi</title>
+    <!-- datatabel -->
+    <link href="<?php echo base_url('assets/backend/datatables/jquery.dataTables.min.css');?>" rel="stylesheet">
+
     <!--icheck-->
     <link rel="stylesheet" href="<?php echo base_url('assets/backend/js/iCheck/skins/minimal/minimal.css');?>" />
     <link rel="stylesheet" href="<?php echo base_url('assets/backend/js/iCheck/skins/square/square.css');?>" />
@@ -66,80 +69,80 @@
 </head>
 
 <body class="cz-shortcut-listen='true'">
-     <!--Untuk logo fosquare-->
-     <div id="header">
-        <h1><a href="<?= base_url(); ?>">SMOP</a></h1>
-    </div>
-    <!--Style Button Right-->
-    <div class="btn-group rightzero">
-      <a class="top_message tip-left" title="" data-original-title="Manage Files"><i class="icon-file"></i></a>
-      <a class="top_message tip-bottom" title="" data-original-title="Manage Users"><i class="icon-user"></i></a>
-      <a class="top_message tip-bottom" title="" data-original-title="Manage Comments"><i class="icon-comment"></i><span class="label label-important">5</span></a>
-      <a class="top_message tip-bottom" title="" data-original-title="Manage Orders"><i class="icon-shopping-cart"></i></a>
-    </div>
-    <div id="user-nav" class="navbar navbar-inverse" style="border-color: #47475c;top:-2px;">
-      <ul class="nav">
-        <li class=""><a href="<?= base_url('mastercms/perusahaan'); ?>"><i class="icon icon-user"></i> <span class="text" style="color: #fff">Pengguna</span></a></li>
-        <li class=""><a title="" href="#view"><i class="icon icon-share-alt"></i> <span class="text" style="color: #fff">Keluar</span></a></li>
-    </ul>
-    <!-- Modal -->
-    </div>
+   <!--Untuk logo fosquare-->
+   <div id="header">
+    <h1><a href="<?= base_url(); ?>">SMOP</a></h1>
+</div>
+<!--Style Button Right-->
+<div class="btn-group rightzero">
+  <a class="top_message tip-left" title="" data-original-title="Manage Files"><i class="icon-file"></i></a>
+  <a class="top_message tip-bottom" title="" data-original-title="Manage Users"><i class="icon-user"></i></a>
+  <a class="top_message tip-bottom" title="" data-original-title="Manage Comments"><i class="icon-comment"></i><span class="label label-important">5</span></a>
+  <a class="top_message tip-bottom" title="" data-original-title="Manage Orders"><i class="icon-shopping-cart"></i></a>
+</div>
+<div id="user-nav" class="navbar navbar-inverse" style="border-color: #47475c;top:-2px;">
+  <ul class="nav">
+    <li class=""><a href="<?= base_url('mastercms/perusahaan'); ?>"><i class="icon icon-user"></i> <span class="text" style="color: #fff">Pengguna</span></a></li>
+    <li class=""><a title="" href="#view"><i class="icon icon-share-alt"></i> <span class="text" style="color: #fff">Keluar</span></a></li>
+</ul>
+<!-- Modal -->
+</div>
 
-    <div class="light-modal" id="view" role="dialog" aria-labelledby="light-modal-label" aria-hidden="false">
-      <div class="light-modal-content  animated zoomInUp">
-        <div class="light-modal-header">
-          <h3 class="light-modal-heading"><i class="fa fa-warning"></i> Keluar</h3>
-      </div>
-      <div class="light-modal-body">
-          Yakin ingin Keluar?
-      </div>
-      <div class="light-modal-footer">
-          <a href="<?php echo base_url('mastercms/login/logout'); ?>" class="btn btn-danger" aria-label="close">Ya</a>&nbsp;&nbsp;&nbsp;
-          <a href="#" class="btn btn-info" aria-label="close">Tidak</a>
-      </div>
-    </div>
-    </div>
-    <!-- end modal -->
-    <?php echo $sidebar; ?>
-    <?php echo $content; ?>
-    <!-- footer -->
-    <div class="row-fluid">
-        <div id="footer" class="span12">SMOP - Sistem Manajemen Otret Presensi &copy; 2018 - by <a href="http://otret.com/" target="_blank">PT OTRET.COM</a> </div>
-    </div>
-    <!--ios7-->
-    <script src="<?php echo base_url('assets/backend/js/ios-switch/switchery.js');?>" ></script>
-    <script src="<?php echo base_url('assets/backend/js/ios-switch/ios-init.js');?>" ></script>
+<div class="light-modal" id="view" role="dialog" aria-labelledby="light-modal-label" aria-hidden="false">
+  <div class="light-modal-content  animated zoomInUp">
+    <div class="light-modal-header">
+      <h3 class="light-modal-heading"><i class="fa fa-warning"></i> Keluar</h3>
+  </div>
+  <div class="light-modal-body">
+      Yakin ingin Keluar?
+  </div>
+  <div class="light-modal-footer">
+      <a href="<?php echo base_url('mastercms/login/logout'); ?>" class="btn btn-danger" aria-label="close">Ya</a>&nbsp;&nbsp;&nbsp;
+      <a href="#" class="btn btn-info" aria-label="close">Tidak</a>
+  </div>
+</div>
+</div>
+<!-- end modal -->
+<?php echo $sidebar; ?>
+<?php echo $content; ?>
+<!-- footer -->
+<div class="row-fluid">
+    <div id="footer" class="span12">SMOP - Sistem Manajemen Otret Presensi &copy; 2018 - by <a href="http://otret.com/" target="_blank">PT OTRET.COM</a> </div>
+</div>
+<!--ios7-->
+<script src="<?php echo base_url('assets/backend/js/ios-switch/switchery.js');?>" ></script>
+<script src="<?php echo base_url('assets/backend/js/ios-switch/ios-init.js');?>" ></script>
 
-    <!--icheck -->
-    <script src="<?php echo base_url('assets/backend/js/iCheck/jquery.icheck.js');?>"></script>
-    <script src="<?php echo base_url('assets/backend/js/icheck-init.js');?>"></script>
+<!--icheck -->
+<script src="<?php echo base_url('assets/backend/js/iCheck/jquery.icheck.js');?>"></script>
+<script src="<?php echo base_url('assets/backend/js/icheck-init.js');?>"></script>
 
-    <!--pickers plugins-->
-    <script type="text/javascript" src="<?php echo base_url('assets/backend/js/bootstrap-datepicker/js/bootstrap-datepicker.js');?>"></script>
-    <script type="text/javascript" src="<?php echo base_url('assets/backend/js/bootstrap-datetimepicker/js/bootstrap-datetimepicker.js');?>"></script>
-    <script type="text/javascript" src="<?php echo base_url('assets/backend/js/bootstrap-daterangepicker/date.js');?>"></script>
-    <script type="text/javascript" src="<?php echo base_url('assets/backend/js/bootstrap-daterangepicker/moment.min.js');?>"></script>
-    <script type="text/javascript" src="<?php echo base_url('assets/backend/js/bootstrap-daterangepicker/daterangepicker.js');?>"></script>
-    <script type="text/javascript" src="<?php echo base_url('assets/backend/js/bootstrap-colorpicker/js/bootstrap-colorpicker.js');?>"></script>
-    <script type="text/javascript" src="<?php echo base_url('assets/backend/js/bootstrap-timepicker/js/bootstrap-timepicker.js');?>"></script>
+<!--pickers plugins-->
+<script type="text/javascript" src="<?php echo base_url('assets/backend/js/bootstrap-datepicker/js/bootstrap-datepicker.js');?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/backend/js/bootstrap-datetimepicker/js/bootstrap-datetimepicker.js');?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/backend/js/bootstrap-daterangepicker/date.js');?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/backend/js/bootstrap-daterangepicker/moment.min.js');?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/backend/js/bootstrap-daterangepicker/daterangepicker.js');?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/backend/js/bootstrap-colorpicker/js/bootstrap-colorpicker.js');?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/backend/js/bootstrap-timepicker/js/bootstrap-timepicker.js');?>"></script>
 
-    <!--pickers initialization-->
-    <script src="<?php echo base_url('assets/backend/js/pickers-init.js');?>"></script>
+<!--pickers initialization-->
+<script src="<?php echo base_url('assets/backend/js/pickers-init.js');?>"></script>
 
-    <!--easy pie chart-->
-    <script src="<?php echo base_url('assets/backend/js/easypiechart/jquery.easypiechart.js');?>"></script>
-    <script src="<?php echo base_url('assets/backend/js/easypiechart/easypiechart-init.js');?>"></script>
+<!--easy pie chart-->
+<script src="<?php echo base_url('assets/backend/js/easypiechart/jquery.easypiechart.js');?>"></script>
+<script src="<?php echo base_url('assets/backend/js/easypiechart/easypiechart-init.js');?>"></script>
 
-    <!--Sparkline Chart-->
-    <script src="<?php echo base_url('assets/backend/js/sparkline/jquery.sparkline.js');?>"></script>
-    <script src="<?php echo base_url('assets/backend/js/sparkline/sparkline-init.js');?>"></script>
+<!--Sparkline Chart-->
+<script src="<?php echo base_url('assets/backend/js/sparkline/jquery.sparkline.js');?>"></script>
+<script src="<?php echo base_url('assets/backend/js/sparkline/sparkline-init.js');?>"></script>
 
 
-    <!--Morris Chart-->
-    <script src="<?php echo base_url('assets/backend/js/morris-chart/morris.js');?>"></script>
-    <script src="<?php echo base_url('assets/backend/js/morris-chart/raphael-min.js');?>"></script>
+<!--Morris Chart-->
+<script src="<?php echo base_url('assets/backend/js/morris-chart/morris.js');?>"></script>
+<script src="<?php echo base_url('assets/backend/js/morris-chart/raphael-min.js');?>"></script>
 
-    <!--Calendar-->
+<!--Calendar-->
 <!--<script src="<?php echo base_url();?>/js/calendar/clndr.js"></script>
 <script src="<?php echo base_url();?>/js/calendar/evnt.calendar.init.js"></script>
 <script src="<?php echo base_url();?>/js/calendar/moment-2.2.1.js"></script>
