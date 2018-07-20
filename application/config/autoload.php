28d556addb8966377ae8bd3781bcdf6b3288ade7
danis16 <daniswara.daniswara@gmail.com>
2018-07-20 17:09:25 +0700

    commit autoload menambahkan library

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database','session', 'pagination','form_validation','Recaptcha');
+$autoload['libraries'] = array('database','session', 'pagination','form_validation','Recaptcha','ssp.class.php');
 
 /*
 | -------------------------------------------------------------------
